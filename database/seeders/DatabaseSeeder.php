e17fe4faefc79bf8d42c542f47b551ae609ec740
Sam L <snow94@mail.com>
2023-04-20 11:27:23 -0300

    TextWidget seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\TextWidget;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
@@ -24,11 +25,29 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('admin123')
         ]);
 
+        $tw = new TextWidget();
+        $tw->key = 'about-us-sidebar';
+        $tw->titulo = '';
+        $tw->activo = true;
+        $tw->save();
+
+        $tw = new TextWidget();
+        $tw->key = 'header';
+        $tw->titulo = '';
+        $tw->activo = true;
+        $tw->save();
+
+        $tw = new TextWidget();
+        $tw->key = 'about-page';
+        $tw->titulo = '';
+        $tw->activo = true;
+        $tw->save();
+
         $adminRole = Role::create(['name' => 'admin']);
 
         $adminUser->assignRole($adminRole);
 
-        // \App\Models\Post::factory(50)->create();
+        \App\Models\Post::factory(50)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
